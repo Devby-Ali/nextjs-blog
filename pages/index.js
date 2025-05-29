d101104c5334e270c6bfc00c46ebbd95818fabab
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-05-29 21:49:39 +0330

    Add test api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
- 
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -11,7 +13,7 @@ export async function getStaticProps() {
     },
   };
 }
- 
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -19,23 +21,26 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm Ali, a front-end developer and software engineer from Iran. You can contact me at <a href="mailto:ali81fordev@gmail.com">Email</a></p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          I'm Ali, a front-end developer and software engineer from Iran. You
+          can contact me at <a href="mailto:ali81fordev@gmail.com">Email</a>
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
